9479dfa168c015689fd6e44f2a5957e4c1fb43e7
capellofranco <capellofranco2@gmail.com>
2026-04-16 19:23:51 -0300

    ES UNA PRUEBA
    prueba intentando hacer el singleton

--- DAL/MP_USUARIO.cs
+++ DAL/MP_USUARIO.cs
@@ -1,6 +1,8 @@
 ﻿using BE;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 
@@ -26,6 +28,28 @@ namespace DAL
         public override int Modificar(USUARIO obj)
         {
             throw new NotImplementedException();
+        }
+
+        public USUARIO ObtenerUsuarioPorNombre(string nombre)
+        {
+            acceso = new ACCESO();
+            acceso.Conectar();
+
+            List<SqlParameter> parametros = new List<SqlParameter>();
+            parametros.Add(acceso.CrearParametro("@nom", nombre));
+
+            DataTable tabla = acceso.Leer("ObtenerUsuPorNom", parametros);
+            acceso.Desconectar();
+
+            if(tabla.Rows.Count == 0)
+            {
+                return null;
+            }
+
+            DataRow fila = tabla.Rows[0];
+
+            USUARIO usuario = new USUARIO();
+            usuario.ID = ;
         }
     }
 }
